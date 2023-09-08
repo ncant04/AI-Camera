422f2de315bad223b578104fc8bbbd38ac3235d0
Nicolò "Nico" Cantori <n.cantori04@gmail.com>
2023-09-08 21:00:31 +0200

    Update MainActivity.java

--- app/src/main/java/it/polocorese/aicamera/MainActivity.java
+++ app/src/main/java/it/polocorese/aicamera/MainActivity.java
@@ -1,18 +1,21 @@
-/* AICamera -- AI powered android app for image recognition
+/* Main entry point for app.
+
    Copyright (C) 2022 Nicolò Cantori
+
+   This file is part of AI Camera.
    
-   This program is free software: you can redistribute it and/or modify
+   AI Camera is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
-   This program is distributed in the hope that is useful,
+   AI Camera is distributed in the hope that is useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+   along with AI Camera.  If not, see <https://www.gnu.org/licenses/>.  */
 
 package it.polocorese.aicamera;
 
